283ca2f7d9a481bc66ffdd6aee86b66d075c7e7f
Alexander Bochkarev <bochkarev_10@mail.ru>
2023-12-22 13:19:03 +0300

    Добавьте файлы проекта.

---
+++ link6-server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace link6_server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
